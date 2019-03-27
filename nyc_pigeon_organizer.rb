a9c8743bd06d3966a893932a232583c3055df5c4
w.h.sinn <w.h.sinn@gmail.com>
2019-03-27 04:42:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(pigeon_data)
   pigeon_data.each_with_object({}) do |(key, h), pigeon|
     h.each do |value, names|
       names.each do |name|
-        pigeon[name]
+        pigeon[name] ||
   
 end
 pigeon_data = {
