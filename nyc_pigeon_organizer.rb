2780a00ef79874a6ed5bc063c41aebb0780057a9
w.h.sinn <w.h.sinn@gmail.com>
2019-03-27 04:44:26 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_data.each_with_object({}) do |(key, h), pigeon|
+    h.each do |value, names|
+      names.each do |name|
+        pigeon[name] ||={}
+        pigeon[name][key] ||=[]
+        pigeon[name][key] << value.to_s
+      end
+    end
+  end
 end
